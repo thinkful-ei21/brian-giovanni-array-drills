7523facb5efba6b6d6f06f0b61686f15acf93d91
giovanni <mormagli@gmail.com>
2018-07-25 13:56:54 -0500

    finished array drill 1

--- arraydrills.js
+++ arraydrills.js
@@ -1,14 +1,42 @@
 'use strict';
 
 //const Memory = require('Memory');
+const Array = require('./practicearray.js');
+// const arr = new Array();
+
+
 
 function main(){
   //Array.SIZE_RATIO = 3;
       
   let arr = new Array();
   
-  arr.push(3);
-  console.log(arr);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+
+  // this should be an array in memory, starting ptr3 = index 0 [5,15,19,45,10]
+ 
+  
+  arr.pop();
+  arr.pop();
+  arr.pop();
+
+  // this should be an array in memory, starting ptr3 = index 0 [5,15]
+
+  console.log(arr.get(0));
+
+  arr.delete();
+  arr.push("tauhida");
+
+  console.log(arr.get(0));
+
+  //the result here is NaN because the memory space in which we are creating our array will only take 64-bit floating point numbers 
+
+
+
 }
 
 main();
--- practicearray.js
+++ practicearray.js
@@ -1,18 +1,41 @@
 'use strict';
 const mem = require('./memory');
 const Memory = new mem();
-console.log(Memory);
+// console.log(Memory);
+
 
 class Array {
   constructor(){
     this.length = 0;
+    this.capacity =0;
     this.ptr = Memory.allocate(this.length);
+    this.RATIO = 3;
   }
+  
 
-  Push(value){
-    this._resize(this.length + 1);
+  push(value){
+    
+    if (this.length +1 > this.capacity){
+      this._resize((this.length +1) * this.RATIO);
+    }
     Memory.set(this.ptr + this.length, value);
     this.length++; 
+  }
+
+  pop(){
+    if(this.length === 0){return new Error('index can\'t be negative');}
+    this.length = this.length-1;
+    return Memory.get(this.ptr + this.length);
+  }
+
+  get(index){
+    if(index < 0 ){return new Error('index can\'t be outside of array');}
+    if(index > this.length -1){return new Error('index can\'t be outside of array');}
+    return Memory.get(this.ptr + index);
+  }
+
+  delete(){
+    this.length = 0;
   }
 
   _resize(size){
@@ -22,11 +45,13 @@ class Array {
     if (this.ptr === null){
       throw new Error ('no more memory');
     }
+    this.capacity = (size);
     Memory.copy(this.ptr, oldPtr, size);
     Memory.free(oldPtr);
   }
 }
 
+module.exports = Array;
 // function main(){
 //   //Array.SIZE_RATIO = 3;
     
